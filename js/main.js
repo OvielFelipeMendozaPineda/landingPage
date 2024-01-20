726ddf56c53007a63d32fb0658fe98e823c01444
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-01-20 10:21:18 -0500

    Arreglado bug de los botones

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-// AOS.init({
-//     offset:1
+// let nav = document.querySelector('nav');
+// document.querySelector('.boton').addEventListener('click', function () {
+//   nav.classList.toggle('active');
 // });
+
+
+
+
+
+
+AOS.init({
+    offset:1
+});
