fd9329ad9e4f96a284b737f79332f9ee971562a4
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-01-21 16:22:49 -0500

    Recreado css

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// let nav = document.querySelector('nav');
-// document.querySelector('.boton').addEventListener('click', function () {
-//   nav.classList.toggle('active');
-// });
-
-
-
-
-
-
-
 AOS.init({
     offset:1
 });
---
+++ js/nav.js
@@ -0,0 +1,4 @@
+let nav = document.querySelector('nav');
+document.querySelector('.boton').addEventListener('click', function () {
+  nav.classList.toggle('active');
+});
