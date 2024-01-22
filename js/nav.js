a9f2a6d97d297a52a480b2e53b00d45e26e0577f
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-01-21 21:59:02 -0500

    Terminado menu res

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-let nav = document.querySelector('nav');
-document.querySelector('.boton').addEventListener('click', function () {
-  nav.classList.toggle('active');
-});
+// let nav = document.querySelector('nav');
+// document.querySelector('.boton').addEventListener('click', function () {
+//   nav.classList.toggle('active');
+// });
